9ad24faac4a21f398d6a20612f41f41ade1828a7
mr-mensah <mr-mensah@gmail.com>
2024-01-26 10:16:16 -0800

    number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def display():
+    """returns a string"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """returns the HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """display c"""
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text='is cool'):
+    """python"""
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    """numbers route"""
+    return f"{n} is a number"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
